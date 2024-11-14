4e7f5cc2713b6824d7979bbf73e8d2aab6796754
serriox <97906837+serriox@users.noreply.github.com>
2024-11-14 20:20:08 +0500

    Minor Update

--- cleaner.sh
+++ cleaner.sh
@@ -1,9 +1,15 @@
 # Update all packages
-yay -Syyu # If you don't have `yay`, use `sudo pacman -Syyu` instead
+yay -Syyu # If you don't have `yay` aur helper, then use `pacman -Syyu`
 
 
-# Remove unnecessary files
+# Remove pacman cache
 sudo pacman -Scc
 sudo pacman -Rcns $(pacman -Qtdq)
+
+# Remove temporary files
+sudo rm -rf "/tmp"
+sudo rm -rf "/var/tmp"
+
+# Remove cache
 sudo rm -rf "/var/cache"
 sudo rm -rf "/home/$USER/.cache"
